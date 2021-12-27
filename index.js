edacc5d317c63f58c134d52c2beb26c110afd016
mohitgupta-byte <mohitguptakarnal1999@gmail.com>
2021-12-27 12:35:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+    
+    const nameEl = document.querySelector("#name");
+const emailEl = document.querySelector("#email");
+const companyNameEl = document.querySelector("#company-name");
+const messageEl = document.querySelector("#message");
+
+const form = document.querySelector("#submit-form");
+
